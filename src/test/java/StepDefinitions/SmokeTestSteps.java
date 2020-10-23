03a7ecb47db60948e5204c9b1711b2f4074c083a
Elma Hodzic <elmahodzic612@gmail.com>
2020-10-23 20:03:16 +0100

    Update SmokeTestSteps

--- src/test/java/StepDefinitions/SmokeTestSteps.java
+++ src/test/java/StepDefinitions/SmokeTestSteps.java
@@ -2,6 +2,7 @@ package StepDefinitions;
 
 import java.util.concurrent.TimeUnit;
 
+import io.cucumber.java.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -47,7 +48,7 @@ public class SmokeTestSteps {
 	
 	@When("user goes to SIGN IN form")
 	public void user_goes_to_SIGN_IN_form() {
-	System.out.println("User goes to SIGN IN form"); 
+	System.out.println("User goes to SIGN IN form");
 	
 	driver.findElement(By.className("login")).click();
 	
@@ -55,7 +56,7 @@ public class SmokeTestSteps {
 	
 	@And("user enters valid credentials in the input fields")
 	public void user_enters_valid_credentials_in_the_input_fields() {
-	System.out.println("User enters valid credentials"); 
+	System.out.println("User enters valid credentials");
 	
 	driver.findElement(By.id("email")).sendKeys("tester072020@gmail.com");
 	driver.findElement(By.id("passwd")).sendKeys("Testing2020");
@@ -63,14 +64,14 @@ public class SmokeTestSteps {
 	
 	@And("user hits enter")
 	public void user_hits_enter() {
-	System.out.println("User hits enter"); 
+	System.out.println("User hits enter");
 	
 	driver.findElement(By.id("SubmitLogin")).click();
 	}
 	
 	@Then("user is successfully logged on My store")
 	public void user_is_successfully_logged_on_My_store() {
-	System.out.println("Welcome to 'My store'"); 
+	System.out.println("Welcome to 'My store'");
 	
 	driver.getPageSource().contains("Welcome to your account. Here you can manage all of your personal information and orders.");
 	
@@ -79,21 +80,21 @@ public class SmokeTestSteps {
 
 	@When("Search for a blouse in the search box")
 	public void Search_for_a_blouse_in_the_search_box() {
-	System.out.println("Search for a blouse"); 
+	System.out.println("Search for a blouse");
 		
 	driver.findElement(By.id("search_query_top")).sendKeys("Blouse");
 	}
 	
 	@And("click on search button")
 	public void click_on_search_button() {
-	System.out.println("Click on search button"); 
+	System.out.println("Click on search button");
 	
 	driver.findElement(By.className("button-search")).click();
 	}
 
 	@And("user adds product to the cart")
 	public void user_adds_product_to_the_cart() {
-		System.out.println("Add to cart"); 
+		System.out.println("Add to cart");
 		
 		WebElement searchBtn = driver.findElement(By.className("product_img_link"));
 
@@ -105,13 +106,13 @@ public class SmokeTestSteps {
 
 	@Then("Product is successfully added users shopping cart")
 	public void Product_is_successfully_added_users_shopping_cart() {
-		System.out.println("Product is successfully added users shopping cart"); 
+		System.out.println("Product is successfully added users shopping cart");
 		
 		driver.getPageSource().contains("Product successfully added to your shopping cart");
 		
 		driver.findElement(By.xpath("//a[@href='http://automationpractice.com/index.php?controller=order']")).click();	
 	}
-	
+
 	@Given("User has added the product to the cart")
 	public void user_has_added_the_product_to_the_cart() {
 		System.out.println("User has added the product to the cart");
@@ -120,31 +121,31 @@ public class SmokeTestSteps {
 
 	@When("User checks all details about shopping cart")
 	public void user_checks_all_details_about_shopping_cart() {
-		System.out.println("User checks all details about shopping cart"); 
+		System.out.println("User checks all details about shopping cart");
 	}
 
 	@And("User clicks to proceed to checkout")
 	public void user_clicks_to_proceed_to_checkout() {
-		System.out.println("User click on procced to checkout button"); 
+		System.out.println("User click on procced to checkout button");
 		driver.findElement(By.className("standard-checkout")).click();
 	}
 
 	@Then("User continues with his order")
 	public void user_continues_with_his_order() {
-		System.out.println("User wants to continue"); 
+		System.out.println("User wants to continue");
 	}
-	
+
 	@Given("User has continued to finish his order")
 	public void user_has_continued_to_finish_his_order() {
-		System.out.println("User has continued to finish his order");	
+		System.out.println("User has continued to finish his order");
 	}
 
 	@When("User checks all details about addresses")
 	public void user_checks_all_details_about_addresses() {
 		System.out.println("User checks all details about addresses");
-		
+
 	}
-	
+
 	@And("User clicks on proceed to checkout")
 	public void user_clicks_on_proceed_to_checkout() {
 		System.out.println("User clicks on proceed to checkout");
@@ -154,9 +155,9 @@ public class SmokeTestSteps {
 	@Then("User continues to finish his order")
 	public void user_continues_to_finish_his_order() {
 		System.out.println("User continues to finish his order");
-		
+
 	}
-	
+
 	@Given("User is on card to check details about shipping")
 	public void user_is_on_card_to_check_details_about_shipping() {
 		System.out.println("User is on card to check details about shipping");
@@ -183,7 +184,7 @@ public class SmokeTestSteps {
 	public void user_is_one_step_to_finish_his_order() {
 		System.out.println("User is one step to finish his order");
 	}
-	
+
 	@Given("User is on card with two payment methods")
 	public void user_is_on_card_to_view_two_payment_methods() {
 		System.out.println("User is on card with two payment methods");
@@ -209,10 +210,9 @@ public class SmokeTestSteps {
 		System.out.println("User has successfully completed the order");
 		driver.findElement(By.xpath("//span[.='I confirm my order']")).click();
 		driver.findElement(By.className("logout")).click();
+
+		driver.close();
 	}
-	
-	
-	
-	
-	
+
+
 }
